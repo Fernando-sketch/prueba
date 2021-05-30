f8f07ef582d6d06628af3333eecb031938295202
Fernando <rios.frc@gmail.com>
2021-05-29 21:26:43 -0500

    menu desplegable

---
+++ menu.js
@@ -0,0 +1,24 @@
+const btnMenu = document.querySelector("#btnMenu");
+const menu = document.querySelector("#menu");
+
+btnMenu.addEventListener("click",function(){
+    menu.classList.toggle("mostrar-menu")
+});
+
+const subMenuBtn = document.querySelectorAll(".btn-submenu");
+for (let i = 0; i < subMenuBtn.length; i++) {
+    subMenuBtn[i].addEventListener("click",function(){
+        console.log("xd");
+        if(window.innerWidth < 768){
+            const subMenu = this.nextElementSibling;
+            const height = subMenu.scrollHeight;
+            if(subMenu.classList.contains("desplegar")){
+                subMenu.classList.remove("desplegar");
+                subMenu.removeAttribute("style");
+            } else{
+                subMenu.classList.add("desplegar");
+                subMenu.style.height = height + "px";
+            }
+        }
+    });
+}
